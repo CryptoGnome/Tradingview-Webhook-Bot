f2d65ca0fa1b3fca3918c1839eb66e7e2309f720
Crypto Gnome <33667144+CryptoGnome@users.noreply.github.com>
2022-06-27 12:54:03 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,147 @@
+import json
+from flask import Flask, render_template, request, jsonify
+from pybit import HTTP
+import time
+
+app = Flask(__name__)
+
+# load config.json
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+
+###############################################################################
+#
+#             This Section is for Exchange Validation
+#
+###############################################################################
+
+use_bybit = False
+if 'BYBIT' in config['EXCHANGES']:
+    print("Bybit is enabled!")
+    use_bybit = True
+    session = HTTP(
+        endpoint='https://api.bybit.com',
+        api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
+        api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
+    )
+
+
+@app.route('/')
+def index():
+    return {'message': 'Server is running!'}
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print("Hook Received!")
+    #data = request.form.to_dict()
+    data = json.loads(request.data)
+    print(data)
+
+
+    if int(data['key']) != config['KEY']:
+        print("Invalid Key, Please Try Again!")
+        return {
+            "status": "error",
+            "message": "Invalid Key, Please Try Again!"
+        }
+
+    # code to place orders here
+    if data['exchange'] == 'bybit':
+
+        if use_bybit:
+            if data['close_position'] == True:
+                print("Closing Position")
+                session.close_position(symbol=data['symbol'])
+            else:
+                if 'cancel_orders' in data:
+                    print("Cancelling Order")
+                    session.cancel_all_active_orders(symbol=data['symbol'])
+                if 'type' in data:
+                    print("Placing Order")
+                    if 'price' in data:
+                        price = data['price']
+                    else:
+                        price = 0
+
+
+                    if data['order_mode'] == 'Both':
+                        take_profit_percent = float(data['take_profit_percent'])/100
+                        stop_loss_percent = float(data['stop_loss_percent'])/100
+                        current_price = session.latest_information_for_symbol(symbol=data['symbol'])['result'][0]['last_price']
+                        if data['side'] == 'Buy':
+                            take_profit_price = round(float(current_price) + (float(current_price) * take_profit_percent), 2)
+                            stop_loss_price = round(float(current_price) - (float(current_price) * stop_loss_percent), 2)
+                        elif data['side'] == 'Sell':
+                            take_profit_price = round(float(current_price) - (float(current_price) * take_profit_percent), 2)
+                            stop_loss_price = round(float(current_price) + (float(current_price) * stop_loss_percent), 2)
+
+
+                        print("Take Profit Price: " + str(take_profit_price))
+                        print("Stop Loss Price: " + str(stop_loss_price))
+
+                        session.place_active_order(symbol=data['symbol'], order_type=data['type'], side=data['side'],
+                                                   qty=data['qty'], time_in_force="GoodTillCancel", reduce_only=False,
+                                                   close_on_trigger=False, price=price, take_profit=take_profit_price, stop_loss=stop_loss_price)
+
+                    if data['order_mode'] == 'Profit':
+                        take_profit_percent = float(data['take_profit_percent'])/100
+                        current_price = session.latest_information_for_symbol(symbol=data['symbol'])['result'][0]['last_price']
+                        if data['side'] == 'Buy':
+                            take_profit_price = round(float(current_price) + (float(current_price) * take_profit_percent), 2)
+                        elif data['side'] == 'Sell':
+                            take_profit_price = round(float(current_price) - (float(current_price) * take_profit_percent), 2)
+
+                        print("Take Profit Price: " + str(take_profit_price))
+                        session.place_active_order(symbol=data['symbol'], order_type=data['type'], side=data['side'],
+                                                   qty=data['qty'], time_in_force="GoodTillCancel", reduce_only=False,
+                                                   close_on_trigger=False, price=price, take_profit=take_profit_price)
+                    if data['order_mode'] == 'Stop':
+                        stop_loss_percent = float(data['stop_loss_percent'])/100
+                        current_price = session.latest_information_for_symbol(symbol=data['symbol'])['result'][0]['last_price']
+                        if data['side'] == 'Buy':
+                            stop_loss_price = round(float(current_price) - (float(current_price) * stop_loss_percent), 2)
+                        elif data['side'] == 'Sell':
+                            stop_loss_price = round(float(current_price) + (float(current_price) * stop_loss_percent), 2)
+
+                        print("Stop Loss Price: " + str(stop_loss_price))
+                        session.place_active_order(symbol=data['symbol'], order_type=data['type'], side=data['side'],
+                                                   qty=data['qty'], time_in_force="GoodTillCancel", reduce_only=False,
+                                                   close_on_trigger=False, price=price, stop_loss=stop_loss_price)
+
+
+
+                    else:
+                        session.place_active_order(symbol=data['symbol'], order_type=data['type'], side=data['side'],
+                                                   qty=data['qty'], time_in_force="GoodTillCancel", reduce_only=False,
+                                                   close_on_trigger=False, price=price)
+
+
+
+        return {
+            "status": "success",
+            "message": "Bybit Webhook Received!"
+        }
+
+
+
+
+
+
+
+    else:
+        print("Invalid Exchange, Please Try Again!")
+        return {
+            "status": "error",
+            "message": "Invalid Exchange, Please Try Again!"
+        }
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
+
